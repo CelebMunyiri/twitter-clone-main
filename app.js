b97efa404d45386f4f870180aefd3a76dddebcbe
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-16 11:52:42 +0300

    modified code in the three files to allow rendering of profile and profile details

--- app.js
+++ app.js
@@ -1,66 +1,65 @@
-
 // Part 1: Fetch user details and build profile page
 fetch('https://jsonplaceholder.typicode.com/users/1')
-.then(response => response.json())
-.then(user => {
-  const profileContainer = document.getElementById('user-profile');
-  const profileHTML = `
-    <div class="user-profile">
-      <img class="user-avatar" src="your_avatar_url_here" alt="User Avatar">
-      <div>
-        <h3>${user.name}</h3>
-        <p>Email: ${user.email}</p>
-        <p>Website: ${user.website}</p>
+  .then(response => response.json())
+  .then(user => {
+    const profileContainer = document.getElementById('user-profile');
+    const profileHTML = `
+      <div class="user-profile">
+        <img class="user-avatar" src="https://media.istockphoto.com/id/1450268558/photo/summer-selfie.jpg?b=1&s=170667a&w=0&k=20&c=Vipvrhj292OQRhFkUTIqKnbtMlSn7EWlIbZRzWE66TU=" alt="User Avatar">
+        <div>
+          <h3>${user.name}</h3>
+          <p>Email: ${user.email}</p>
+          <p>Website: ${user.website}</p>
+        </div>
       </div>
-    </div>
-  `;
-  profileContainer.innerHTML = profileHTML;
-});
+    `;
+    profileContainer.innerHTML = profileHTML;
+  });
 
 // Part 2: Fetch user posts
 fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
-.then(response => response.json())
-.then(posts => {
-  const postsContainer = document.getElementById('posts');
-  let postsHTML = '';
-  posts.forEach(post => {
-    postsHTML += `
-      <div class="post">
-        <h4>${post.title}</h4>
-        <p>${post.body}</p>
-        <button class="show-comments-btn" data-post-id="${post.id}">Show Comments</button>
-        <div class="comments" id="comments-${post.id}"></div>
-      </div>
-    `;
-  });
-  postsContainer.innerHTML = postsHTML;
+  .then(response => response.json())
+  .then(posts => {
+    const postsContainer = document.getElementById('posts');
+    let postsHTML = '';
+    posts.forEach(post => {
+      postsHTML += `
+        <div class="post">
+          <h4>${post.title}</h4>
+          <p>${post.body}</p>
+          <button class="show-comments-btn" data-post-id="${post.id}">Show Comments</button>
+          <div class="comments" id="comments-${post.id}"></div>
+        </div>
+      `;
+    });
+    postsContainer.innerHTML = postsHTML;
 
-  // Part 3: Show/hide comments on button click
-  const showCommentsButtons = document.getElementsByClassName('show-comments-btn');
-  Array.from(showCommentsButtons).forEach(button => {
-    button.addEventListener('click', () => {
-      const postId = button.getAttribute('data-post-id');
-      const commentsContainer = document.getElementById(`comments-${postId}`);
-      const commentsDisplay = commentsContainer.style.display;
+    // Part 3: Show/hide comments on button click
+    const showCommentsButtons = document.getElementsByClassName('show-comments-btn');
+    Array.from(showCommentsButtons).forEach(button => {
+      button.addEventListener('click', () => {
+        const postId = button.getAttribute('data-post-id');
+        const commentsContainer = document.getElementById(`comments-${postId}`);
+        const commentsDisplay = commentsContainer.style.display;
 
-      if (commentsDisplay === 'none') {
-        fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
-          .then(response => response.json())
-          .then(comments => {
-            let commentsHTML = '';
-            comments.forEach(comment => {
-              commentsHTML += `
-                <p><strong>${comment.name}</strong>: ${comment.body}</p>
-              `;
+        if (commentsDisplay === 'none') {
+          fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+            .then(response => response.json())
+            .then(comments => {
+              let commentsHTML = '';
+              comments.forEach(comment => {
+                commentsHTML += `
+                  <p><strong>${comment.name}</strong>: ${comment.body}</p>
+                `;
+              });
+              commentsContainer.innerHTML = commentsHTML;
+              commentsContainer.style.display = 'block';
+              button.textContent = 'Hide Comments';
             });
-            commentsContainer.innerHTML = commentsHTML;
-            commentsContainer.style.display = 'block';
-            button.textContent = 'Hide Comments';
-          });
-      } else {
-        commentsContainer.style.display = 'none';
-        button.textContent = 'Show Comments';
-      }
+        } else {
+          commentsContainer.style.display = 'none';
+          button.textContent = 'Show Comments';
+        }
+      });
     });
   });
-});
