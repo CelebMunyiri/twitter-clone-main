5008d82c2fdf53fbf4946cbd204c2c1a16cc4bd3
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-17 18:23:18 +0300

    made some changes pn the styling of post

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 // Part 1: Fetch user details and build profile page from the API provided,
-fetch('https://jsonplaceholder.typicode.com/users/1')
+for(let i=0;i<=10;i++)
+fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(user => {
     const profileContainer = document.getElementById('user-profile');
     const profileHTML = `
+    <div class="profile">
+    <h2 style="color: rgb(24, 4, 4);">Twitter Clone</h2>
+     <select name="" id="">
+        <option value="">${user[0].name}</option>
+        <option value="">${user[1].name}</option>
+        <option value="">${user[2].name}</option>
+        <option value="">${user[3].name}</option>
+        <option value="">${user[4].name}</option>
+        <option value="">${user[5].name}</option>
+        <option value="">${user[6].name}</option>
+        <option value="">${user[7].name}</option>
+        <option value="">${user[8].name}</option>
+        
+    </select> 
+  </div>
       <div class="user-profile">
         <img class="user-avatar" src="https://media.istockphoto.com/id/1450268558/photo/summer-selfie.jpg?b=1&s=170667a&w=0&k=20&c=Vipvrhj292OQRhFkUTIqKnbtMlSn7EWlIbZRzWE66TU=" alt="User Avatar">
         <div>
-          <h3>${user.name}</h3>
-          <p> ${user.email}</p>
-          <p>Website: ${user.website}</p>
-          <iconify-icon icon="carbon:location" style="color: gray;"></iconify-icon> New york, USA
+          <h3>${user[1].name}</h3>
+          <p> ${user[1].email}</p>
+          <p>Website: ${user[1].website}</p>
+          <iconify-icon icon="carbon:location" style="color: gray;"></iconify-icon> Gwenborough
          
           <iconify-icon icon="solar:calendar-linear" style="color: gray;"></iconify-icon> Joined December 2020
           
@@ -30,11 +46,12 @@ fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
     posts.forEach(post => {
       postsHTML += `
         <div class="post">
-       
-        <img src='https://cdn.pixabay.com/photo/2023/07/13/08/50/squirrel-8124227_640.jpg' alt='avatar image'>
+       <div>
+        <img src='https://media.istockphoto.com/id/1450268558/photo/summer-selfie.jpg?b=1&s=170667a&w=0&k=20&c=Vipvrhj292OQRhFkUTIqKnbtMlSn7EWlIbZRzWE66TU=' alt="User image'>
        <div class="intro"
-        <h3> General Matejagwo <iconify-icon icon="ic:round-verified" style="color: blue;"></iconify-icon></h3><iconify-icon icon="skill-icons:twitter" style="color: gray;"></iconify-icon>
+        <h3> Leanne Graham <iconify-icon icon="ic:round-verified" style="color: blue;"></iconify-icon></h3><iconify-icon icon="skill-icons:twitter" style="color: gray;"></iconify-icon>
         </div>
+        
         @general_matejagwo
         
           <h4>${post.title}</h4>
